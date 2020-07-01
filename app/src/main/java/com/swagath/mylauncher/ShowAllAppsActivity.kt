6f6f91640e96f98685c1c56068667168232fcec5
Swagath <swagath.naik@gmail.com>
2020-07-01 14:10:27 +0530

    Add ShowAllAppsActivity

---
+++ app/src/main/java/com/swagath/mylauncher/ShowAllAppsActivity.kt
@@ -0,0 +1,11 @@
+package com.swagath.mylauncher
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ShowAllAppsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_all_apps)
+    }
+}
