d7898a7ff5190840dd8a24e4ba01bc1f1d8fd8a1
Swagath <swagath.naik@gmail.com>
2020-07-26 00:05:21 +0530

    Added VoiceListenerActivity

---
+++ app/src/main/java/com/swagath/mylauncher/VoiceListenerActivity.kt
@@ -0,0 +1,11 @@
+package com.swagath.mylauncher
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class VoiceListenerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_voice_listener)
+    }
+}
